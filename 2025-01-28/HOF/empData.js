352b401607180d51b3d9b3c0d4eedbe250221a13
Nabina Upreti <nabina419@gmail.com>
2025-01-28 13:31:25 +0545

    Refactored data transformation logic using map()

---
+++ 2025-01-28/HOF/empData.js
@@ -0,0 +1,187 @@
+export const employeeData = [
+    {
+      id: 1,
+      date: "2021-01-01",
+      name: "Ram",
+      workedHours: 8,
+    },
+    {
+      id: 2,
+      date: "2021-01-02",
+      name: "Ram",
+      workedHours: 7,
+    },
+    {
+      id: 3,
+      date: "2021-01-03",
+      name: "Ram",
+      workedHours: 9,
+    },
+    {
+      id: 4,
+      date: "2021-01-04",
+      name: "Ram",
+      workedHours: 6,
+    },
+    {
+      id: 5,
+      date: "2021-01-05",
+      name: "Ram",
+      workedHours: 8,
+    },
+    {
+      id: 6,
+      date: "2021-01-06",
+      name: "Ram",
+      workedHours: 5,
+    },
+    {
+      id: 7,
+      date: "2021-01-07",
+      name: "Ram",
+      workedHours: 7,
+    },
+    {
+      id: 8,
+      date: "2021-01-08",
+      name: "Ram",
+      workedHours: 8,
+    },
+    {
+      id: 9,
+      date: "2021-01-09",
+      name: "Ram",
+      workedHours: 6,
+    },
+    {
+      id: 10,
+      date: "2021-01-10",
+      name: "Ram",
+      workedHours: 9,
+    },
+  
+    {
+      id: 11,
+      date: "2021-01-01",
+      name: "Shyam",
+      workedHours: 7,
+    },
+    {
+      id: 12,
+      date: "2021-01-02",
+      name: "Shyam",
+      workedHours: 8,
+    },
+    {
+      id: 13,
+      date: "2021-01-03",
+      name: "Shyam",
+      workedHours: 6,
+    },
+    {
+      id: 14,
+      date: "2021-01-04",
+      name: "Shyam",
+      workedHours: 9,
+    },
+    {
+      id: 15,
+      date: "2021-01-05",
+      name: "Shyam",
+      workedHours: 7,
+    },
+    {
+      id: 16,
+      date: "2021-01-06",
+      name: "Shyam",
+      workedHours: 8,
+    },
+    {
+      id: 17,
+      date: "2021-01-07",
+      name: "Shyam",
+      workedHours: 6,
+    },
+    {
+      id: 18,
+      date: "2021-01-08",
+      name: "Shyam",
+      workedHours: 9,
+    },
+    {
+      id: 19,
+      date: "2021-01-09",
+      name: "Shyam",
+      workedHours: 8,
+    },
+    {
+      id: 20,
+      date: "2021-01-10",
+      name: "Shyam",
+      workedHours: 7,
+    },
+  
+    {
+      id: 21,
+      date: "2021-01-01",
+      name: "Hari",
+      workedHours: 6,
+    },
+    {
+      id: 22,
+      date: "2021-01-02",
+      name: "Hari",
+      workedHours: 8,
+    },
+    {
+      id: 23,
+      date: "2021-01-03",
+      name: "Hari",
+      workedHours: 9,
+    },
+    {
+      id: 24,
+      date: "2021-01-04",
+      name: "Hari",
+      workedHours: 7,
+    },
+    {
+      id: 25,
+      date: "2021-01-05",
+      name: "Hari",
+      workedHours: 6,
+    },
+    {
+      id: 26,
+      date: "2021-01-06",
+      name: "Hari",
+      workedHours: 8,
+    },
+    {
+      id: 27,
+      date: "2021-01-07",
+      name: "Hari",
+      workedHours: 9,
+    },
+    {
+      id: 28,
+      date: "2021-01-08",
+      name: "Hari",
+      workedHours: 7,
+    },
+    {
+      id: 29,
+      date: "2021-01-09",
+      name: "Hari",
+      workedHours: 8,
+    },
+    {
+      id: 30,
+      date: "2021-01-10",
+      name: "Hari",
+      workedHours: 6,
+    },
+  ]
+  
+
+
